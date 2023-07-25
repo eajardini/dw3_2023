720ed8cabd56d465a16970bbd770ddbc46936311
Evandro Jardini <eajardini@ifsp.edu.br>
2023-07-25 17:04:25 -0300

    Adicionado tabelas exercícios

--- dw3backend/databaseConfig.sql
+++ dw3backend/databaseConfig.sql
@@ -41,9 +41,39 @@ create table IF NOT EXISTS usuarios (
     deleted boolean DEFAULT false
 );
 
-CREATE EXTENSION pgcrypto;
+CREATE EXTENSION if NOT EXISTS pgcrypto;
 
 insert into usuarios values 
     (default, 'admin', crypt('admin', gen_salt('bf'))), -- senha criptografada com bcrypt
     (default, 'qwe', crypt('qwe', gen_salt('bf'))) -- senha criptografada com bcrypt
 ON CONFLICT DO NOTHING;
+
+-- Usado para exercícios
+
+create table IF NOT EXISTS clientes (
+    clienteid bigserial constraint pk_clientes PRIMARY KEY,
+    codigo varchar(50) UNIQUE,
+    nome VARCHAR(60),
+    endereco VARCHAR(50),
+    ativo boolean,
+    deleted boolean DEFAULT false
+);
+
+insert into clientes values 
+    (default, 'CLI01', 'João da Silva', 'Rua A1', true),
+    (default, 'CLI02', 'Marcia Almeida', 'Rua B2', true)
+    ON CONFLICT DO NOTHING;
+
+create table IF NOT EXISTS pedidos (
+    pedidoid bigserial constraint pk_pedidos PRIMARY KEY,
+    numero bigint UNIQUE,
+    data DATE,
+    valortotal numeric(9,2),
+    clienteid bigint constraint fk_pedido_cliente REFERENCES clientes,    
+    deleted boolean DEFAULT false
+);
+
+insert into pedidos values 
+ (default, 234, '2020-01-31', 6891.60, (SELECT clienteid from CLIENTES where codigo = 'CLI01'))
+ ON CONFLICT DO NOTHING;
+
